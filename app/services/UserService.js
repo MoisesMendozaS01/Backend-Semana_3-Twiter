91a9874af9bd3e5f097e3bc246c3d0102a88d329
= <=>
2022-05-03 15:43:41 -0500

    Paso 10.1: Servicios: Requerimiento 2 (Agregando funcion estática 'getinfo' para lista de atributos de usuario, pasando prueba de unidad)

---
+++ app/services/UserService.js
@@ -0,0 +1,13 @@
+const User=require("./../models/User")
+
+class UserService{
+    static create(id,username,name){
+        return new User(id,username,name,"Sin bio")
+    }
+    static getInfo(user){
+        return [user.id,user.username,user.name,user.bio]
+    }
+}
+
+console.log(this.getInfo)
+module.exports = UserService
