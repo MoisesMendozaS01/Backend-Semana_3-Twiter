d41654b349784f17100eefd201e874e350847849
= <=>
2022-05-03 20:31:53 -0500

    Paso 12: Servicios: Requerimiento 3 (Prueba de unidad para obtener nombres de usuarios)

--- test/services/UserServices.test.js
+++ test/services/UserServices.test.js
@@ -24,4 +24,14 @@ describe("Test for UserService",()=>{
 
         expect(user.username).toBe("carlog")
     })
+    test("Servicios: Requerimiento 4: username list",()=>{
+        const user1 = UserService.create(1,"carlogilmar","Carlo")
+        const user2 = UserService.create(2,"MoisesMendozaS01","Moises")
+        const user3 = UserService.create(3,"romarpla","Rodrigo")
+
+        const usernames = UserService.getAllUsernames([user1,user2,user3])
+        expect(usernames).toContain("carlogilmar")
+        expect(usernames).toContain("MoisesMendozaS01")
+        expect(usernames).toContain("romarpla")
+    })
 })
