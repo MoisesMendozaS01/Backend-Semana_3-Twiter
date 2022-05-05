44dc3cd3e70c86162b8ba0f71db4577eb43618a4
= <=>
2022-05-04 21:33:47 -0500

    Paso 14: View: Requerimiento 2: Agregando prueba de unidad de payload contenga en alguna de las llaves username, name, o id un valor en null

--- test/view/UserView.test.js
+++ test/view/UserView.test.js
@@ -7,4 +7,10 @@ describe("Test for Userview",()=>{
         //htpps://jest.io/doc/using-matchers#strings
         expect(result.error).toMatch('/payload no existe/')
     })
+
+    test("View: requerimiento 2: Return an error object when try to create a new user with a payload with invalid properties",()=>{
+        const payload ={username:null,name:12,id:"id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
 })
